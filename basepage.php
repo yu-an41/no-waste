a6d72196615ca6fafecf501fb74c5f1ce674b61a
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-09-16 17:28:40 +0800

    basepage新增小卡

--- basepage.php
+++ basepage.php
@@ -8,5 +8,64 @@ $pageName = 'base';
 <div class="container">
     \ 惜食戰士GOGO！ /
 </div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">商品</h5>
+                    <a href="#" class="btn btn-primary">商品列表</a>
+                    <a href="#" class="btn btn-primary">新增商品</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">商品</h5>
+                    <a href="#" class="btn btn-primary">商品列表</a>
+                    <a href="#" class="btn btn-primary">新增商品</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">商品</h5>
+                    <a href="#" class="btn btn-primary">商品列表</a>
+                    <a href="#" class="btn btn-primary">新增商品</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">商品</h5>
+                    <a href="#" class="btn btn-primary">商品列表</a>
+                    <a href="#" class="btn btn-primary">新增商品</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">商品</h5>
+                    <a href="#" class="btn btn-primary">商品列表</a>
+                    <a href="#" class="btn btn-primary">新增商品</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">商品</h5>
+                    <a href="#" class="btn btn-primary">商品列表</a>
+                    <a href="#" class="btn btn-primary">新增商品</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php include __DIR__ . './parts/scripts.php'; ?>
 <?php include __DIR__ . './parts/html-foot.php'; ?>
