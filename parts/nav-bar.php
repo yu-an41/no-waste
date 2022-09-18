a8995bd26a8740f37a6b02bb030994fa9af7ec79
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-09-18 11:37:18 +0800

    新增購物車icon

--- parts/nav-bar.php
+++ parts/nav-bar.php
@@ -34,12 +34,14 @@
                     <li class="nav-item <?= $pageName == 'forum' ? 'active' : '' ?>">
                         <a class="nav-link <?= $pageName == 'product' ? 'active' : '' ?>" href="#">論壇</a>
                     </li>
-                    <li class="nav-item <?= $pageName == 'cart' ? 'active' : '' ?>">
-                        <a class="nav-link" href="#">購物車</a>
-                    </li>
                 </ul>
                 <ul class="navbar-nav mb-2 mb-lg-0">
                     <?php if (empty($_SESSION['user1'])) : ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="fa-solid fa-cart-shopping"></i>
+                            </a>
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link" href="login-api.php">登入</a>
                         </li>
@@ -47,6 +49,11 @@
                             <a class="nav-link" href="#">註冊</a>
                         </li>
                     <?php else : ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="fa-regular fa-cart-shopping"></i>
+                            </a>
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link"><?= $_SESSION['user1']['nickname'] ?></a>
                         </li>
