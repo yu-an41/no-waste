dd4a839499d45e3b9d9ce0994bc6ff10246bf834
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-09-19 14:05:18 +0800

    admin validation

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+    require __DIR__ . '/parts/connect_db.php';
+    $pageName = 'logout';
+?>
+<?php
+include __DIR__ . '/parts/html-head.php'; ?>
+<?php
+include __DIR__ . '/parts/nav-bar.php'; ?>
+
+<?php
+include __DIR__ . '/parts/scripts.php'; ?>
+<?php
+include __DIR__ . '/parts/html-foot.php'; ?>
