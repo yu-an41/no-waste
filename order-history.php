587f587c7309084ca81ba34a0191c908ff32fecc
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-09-19 15:49:43 +0800

    add diff links

--- order-history.php
+++ order-history.php
@@ -1,15 +1,15 @@
 <?php
 require __DIR__ . '/parts/connect_db.php';
-$pageName = 'orderHistory';
+$pageName = 'orders';
 
 $perPage = 5;
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
 
 
-$t_sql = "SELECT COUNT() FROM order-history";
-$totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
+$o_sql = "SELECT COUNT(1) FROM `order-history`";
+$totalRows = $pdo->query($o_sql)->fetch(PDO::FETCH_NUM)[0];
 
-$totalPages = ceil($totalRows / $totalPages);
+$totalPages = ceil($totalRows / $perPage);
 
 $rows = [];
 
@@ -23,7 +23,7 @@ if ($totalRows) {
         exit;
     }
     $sql = sprintf(
-        "SELECT FTOM order-history ORDER BY created_at LIMIT %s, %s",
+        "SELECT * FROM `order-history` ORDER BY `created_at` LIMIT %s, %s",
         ($page - 1) * $perPage,
         $perPage
     );
@@ -38,13 +38,51 @@ $output = [
     'perPage' => $perPage,
 ];
 
-echo json_encode($output);
-exit;
+// echo json_encode($output); exit;
 ?>
 <?php
-include __DIR__ . '/parts/html-head.php'; ?>
+require __DIR__ . '/parts/html-head.php'; ?>
 <?php
 include __DIR__ . '/parts/nav-bar.php'; ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <li class="page-item <?= 1 === $page ? 'disabled' : '' ?>">
+                        <a class="page-link" href="#">
+                            <i class="fa-solid fa-arrow-left"></i>
+                        </a>
+                    </li>
+                    <?php for ($i = $page - 5; $i <= $page + 5; $i++) :
+                        if ($i >= 1 and $i <= $totalPages) :
+                    ?>
+                            <li class="page-item <?= $i === $page ? 'active' : '' ?>">
+                                <a class="page-link" href="?page=<?= $i ?>">
+                                    <?= $i ?>
+                                </a>
+                            </li>
+                    <?php endif;
+                    endfor; ?>
+                    <li class="page-item <?= $totalPgaes === $page ? 'disabled' : '' ?>">
+                        <a class="page-link" href="?page=<?= $page + 1 ?>">
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
 
+<?php if (!empty($_SESSION['admin'])) {
+    include __DIR__ . 'order-history-admin.php';
+} else if (!empty($_SESSION['user'])) {
+    include __DIR__ . 'order-history-user.php';
+} else {
+    include __DIR__ . 'order-history-nouser.php';
+}
+?>
+</div>
 <?php
 include __DIR__ . '/parts/scripts.php'; ?>
+<?php include __DIR__ . '/parts/html-foot.php'; ?>
